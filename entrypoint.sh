e5e63a4df5ba87613146484e6973f2b8758d3acb
nancy-singha <100945508+nancy-singha@users.noreply.github.com>
2023-02-12 18:23:13 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
 
-export SPARK_MASTER_PORT=7077
-
-# run spark 
-cd /usr/local/spark/bin
-
-. "/load-spark-env.sh"
-
-if [[ "$SPARK_ROLE" -eq "SPARK-MASTER" ]]; then
-	./spark-class org.apache.spark.deploy.master.Master
-	echo "1---master"
-elif [[ "$SPARK_ROLE" -eq "SPARK-WORKER" ]]; then
-	./spark-class org.apache.spark.deploy.worker.Worker spark://`hostname`:$SPARK_MASTER_PORT
-	echo "2---worker"
-fi
-
-CMD=${1:-"exit 0"}
-if [[ "$CMD" -eq "-d" ]];
-then
-	service sshd stop
-	/usr/sbin/sshd -D -d
-else
-	/bin/bash -c "$*"
-fi
+echo "Inside EntryPoint.sh"
