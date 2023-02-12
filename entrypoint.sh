0c8bda20a340535ff7875d80403c52c4f91eedc0
nancy-singha <100945508+nancy-singha@users.noreply.github.com>
2023-02-12 18:05:42 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-export SPARK_MASTER_PORT=7077;
+export SPARK_MASTER_PORT=7077
 
 # run spark 
 cd /usr/local/spark/bin
 
 . "/load-spark-env.sh"
 
-if [[ "SPARK_ROLE" -eq "SPARK-MASTER" ]];
-then
-    ./spark-class org.apache.spark.deploy.master.Master
+if [[ "$SPARK_ROLE" -eq "SPARK-MASTER" ]]; then
+	./spark-class org.apache.spark.deploy.master.Master
 	echo "1---master"
-fi
-
-if [[ "SPARK_ROLE" -eq "SPARK-WORKER" ]];
-then
-    ./spark-class org.apache.spark.deploy.worker.Worker spark://`hostname`:$SPARK_MASTER_PORT
+elif [[ "$SPARK_ROLE" -eq "SPARK-WORKER" ]]; then
+	./spark-class org.apache.spark.deploy.worker.Worker spark://`hostname`:$SPARK_MASTER_PORT
 	echo "2---worker"
+else
+then
+	echo "Please select appropriate role. [SPARK-MASTER OR SPARK-WORKER]"
 fi
 
 CMD=${1:-"exit 0"}
