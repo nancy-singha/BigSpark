3966a0aa05c46eabcf9d90f096a4d6caa8f2ad71
nancy-singha <100945508+nancy-singha@users.noreply.github.com>
2023-02-12 16:51:43 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements. See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 export SPARK_MASTER_PORT=7077
-
+echo "xx------------1"
 # run spark 
 cd /usr/local/spark/bin
 
@@ -24,10 +9,12 @@ cd /usr/local/spark/bin
 if [ "$SPARK_ROLE" == "SPARK-MASTER" ];
 then
 	./spark-class org.apache.spark.deploy.master.Master
+	echo "xx-----------2----master"
 
 elif [ "$SPARK_ROLE" == "SPARK-WORKER" ];
 then
 	./spark-class org.apache.spark.deploy.worker.Worker spark://`hostname`:$SPARK_MASTER_PORT
+	echo "xx-----------3------worker"
 else
 then
 	echo "Please select appropriate role. [SPARK-MASTER OR SPARK-WORKER]"
