e7fec8618f3a0ffc09c5360569d93beda0bcc185
nancy-singha <100945508+nancy-singha@users.noreply.github.com>
2023-02-12 17:50:06 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-export SPARK_MASTER_PORT=7077
-echo "xx------------1"
+
+export SPARK_MASTER_PORT=7077;
+
 # run spark 
 cd /usr/local/spark/bin
 
 . "/load-spark-env.sh"
 
-if [ "$SPARK_ROLE" == "SPARK-MASTER" ];
+if [[ "SPARK_ROLE" -eq "SPARK-MASTER" ]];
 then
-	./spark-class org.apache.spark.deploy.master.Master
-	echo "xx-----------2----master"
+    ./spark-class org.apache.spark.deploy.master.Master
+	echo "1---master"
+fi
 
-elif [ "$SPARK_ROLE" == "SPARK-WORKER" ];
+if [[ "SPARK_ROLE" -eq "SPARK-WORKER" ]];
 then
-	./spark-class org.apache.spark.deploy.worker.Worker spark://`hostname`:$SPARK_MASTER_PORT
-	echo "xx-----------3------worker"
-else
-then
-	echo "Please select appropriate role. [SPARK-MASTER OR SPARK-WORKER]"
+    ./spark-class org.apache.spark.deploy.worker.Worker spark://`hostname`:$SPARK_MASTER_PORT
+	echo "2---worker"
 fi
 
 CMD=${1:-"exit 0"}
-if [[ "$CMD" == "-d" ]];
+if [[ "$CMD" -eq "-d" ]];
 then
 	service sshd stop
 	/usr/sbin/sshd -D -d
